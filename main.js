267079ab47d2cd944ff9f4ceb40272b6ab842c27
Ramon Alejandro Aguirre Romero <73568821+LexAguirre@users.noreply.github.com>
2022-09-03 18:12:44 -0500

    Moved

---
+++ main.js
@@ -0,0 +1,22 @@
+let s1 = 'Hola, Mundo!';
+
+let a = 5;
+let b = 7;
+let r = a + b;
+let nombre = 'Juan';
+let edad = 18;
+
+//5 + 7 = 12
+//String
+console.log(a + ' + ' + b + ' = ' + r);
+
+console.log('Hola el es ' + nombre + ' y tiene ' + edad + ' años')
+
+//Plantillas
+console.log(`${a} + ${b} = ${r}`);
+//se puede dejar una expresion 
+console.log(`${a} + ${b} = ${a + b}`);
+
+console.log(`Hola el es ${nombre} y tiene ${edad} años`);
+
+console.log(s1);
